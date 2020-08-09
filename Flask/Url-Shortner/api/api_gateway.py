dfff6a1bc0311c2c0d0d443787e3ff263ddb0a32
Ankit Pal <ankitpal181@gmail.com>
2020-08-09 19:28:01 +0530

    Sequence Diagrams Added To Documentation

--- Flask/Url-Shortner/api/api_gateway.py
+++ Flask/Url-Shortner/api/api_gateway.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 
+# Importing required files
 import json
-from flask import request, Response, make_response
+from flask import request, Response
 from system import app
 from controllers.api import APIController as api
 from controllers.user import UserController as user
 from controllers.rate_limiter import RateLimiterController as limit
 from controllers.logger import LoggerController as logger
 
+# Function generates a reponse object
 def generateResponse(status="200 OK", content_type="application/json", cache_control="no-cache", message=""):
     response = Response()
     response.status = status
@@ -16,23 +18,30 @@ def generateResponse(status="200 OK", content_type="application/json", cache_con
     response.response = message
     return response
 
+# API access endpoint
 @app.route('/api', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def run_request():
     api_key = request.args.get('api-key')
     curr_user = user()
+    # Authenticating user
     if curr_user.authenticate_user(api_key):
+        # Checking request method
         if request.method == 'GET':
             url = request.args.get('url')
+            # Checking if user is authorized to access the requested resource
             if curr_user.authorize_user(url):
                 limitter = limit()
                 limitter.user = api_key
-
+                
+                # Checking limit for current user
                 if limitter.check_limit() < 10:
                     limitter.update_limit()
 
+                    # Create API object
                     api_object = api()
                     read = request.args.get('read')
 
+                    # Proccess user's request
                     if read.lower() == "short":
                         api_object.long_url = url
                         result = api_object.readUrl("short")
@@ -43,6 +52,7 @@ def run_request():
                         msg = "<html><head><title>Bad Request</title></head><body><h1>Bad Request</h1><p>Wrong value assigned to read parameter</p></body></html>"
                         response = generateResponse("400 Bad Request", "application/html", message=msg)
                     
+                    # Generate response
                     if result:
                         msg = json.dumps({'result': result})
                         response = generateResponse(cache_control="public", message=msg)
@@ -55,6 +65,8 @@ def run_request():
             else:
                 msg = "<html><head><title>Forbidden</title></head><body><h1>Forbidden</h1><p>User do not have permission to access this resource</p></body></html>"
                 response = generateResponse("403 Forbidden", "application/html", message=msg)
+            
+            # Return API response
             return response
         
         if request.method == 'POST':
